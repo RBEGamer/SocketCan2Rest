e34fb40abaa0f15c07555ffe75ab04c7edb6bb67
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2022-04-15 21:36:32 +0200

    .

---
+++ service/install_service.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+
+
+echo "-- INSTALL CAN2REST SERVICE ----"
+cd "$(dirname "$0")"
+
+ CWD="$(pwd)"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [[ -z "${CAN2REST_INSTALL_DIR}" ]]; then
+  export CAN2REST_INSTALL_DIR=/usr/can2rest
+fi
+
+echo "INSTALLATION DIR"
+echo "${CAN2REST_INSTALL_DIR}"
+
+
+# CREATE SERVICE FILE
+pwd
+cp ./can2rest.service.TEMPLATE ./can2rest.service
+sed -i -e 's|_CAN2REST_INSTALL_DIR_|'$CAN2REST_INSTALL_DIR'|' ./can2rest.service
+
+
+# INSTALL SERVICE
+cp ./can2rest.service /etc/systemd/system/
+cat /etc/systemd/system/can2rest.service
+systemctl daemon-reload
+
+
+
+# MODIFY CAN2REST START FILE
+cp ./start_can2rest.sh.TEMPLATE ./start_can2rest.sh
+chmod +x ./start_can2rest.sh
+sed -i -e 's|_CAN2REST_INSTALL_DIR_|'$CAN2REST_INSTALL_DIR'|' ./start_can2rest.sh
+cat ./start_can2rest.sh
+
+
+
+# COPY UDEV RULE
+cp ./99-can2rest.rules /etc/udev/rules.d
+udevadm control --reload-rules
+udevadm trigger
+# BUILD
+bash ../src/build.sh
+
+echo $CWD
+cd $CWD
+
+
+# FINALLY COPY ALL OVER
+mkdir -p $CAN2REST_INSTALL_DIR
+
+FILE=../src/can2rest
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    cd ../src/
+    cmake .
+    make
+    cd ../service/
+fi
+
+cp ../src/can2rest $CAN2REST_INSTALL_DIR/can2rest
+cp ./start_can2rest.sh $CAN2REST_INSTALL_DIR/start_can2rest.sh
+
+
+
+
+# ENABLE SERVICE
+systemctl enable can2rest
+sleep 10
+systemctl status can2rest
--- src/install_can_utils_and_usbtin.sh
+++ src/install_can_utils_and_usbtin.sh
@@ -9,18 +9,6 @@ fi
 sudo apt install automake autoconf libtool cmake -y
 
 
-
-
-echo "INSTALLATION DIR"
-echo "${CAN2RESTDIR}"
-
-# INSTALL USBtin RULE
-cp ./99-usbtin.rules /etc/udev/rules.d/
-udevadm control --reload-rules
-udevadm trigger
-
-
-
 #  NET-TOOLS
 sudo apt install net-tools
 # OR USE SOURCE
@@ -75,37 +63,3 @@ make
 make install
 rm CMakeCache.txt && rm -Rf CMakeFiles/
 cd ..
-
-# BUILD CAN2REST PROGRAM
-pwd
-cd ./can2rest
-rm CMakeCache.txt && rm -Rf CMakeFiles/
-cmake .
-make
-chmod +x ./can2rest
-rm -Rf ./CMakeFiles
-rm -Rf ./cmake-build-debug
-rm -f ./CMakeCache.txt
-rm -f ./cmake_install.cmake
-rm -f Makefile
-cd ..
-
-
-# CREATE SERVICE FILE
-pwd
-cp ./can2rest_template.service ./can2rest.service
-sed -i -e 's|_CAN2RESTDIR_|'$CAN2RESTDIR'|' ./can2rest.service
-
-
-# INSTALL SERVICE
-cp ./can2rest.service /etc/systemd/system/
-cat /etc/systemd/system/can2rest.service
-systemctl daemon-reload
-# ENABLE SERVICE
-systemctl enable can2rest
-
-
-# MODIFY CAN2REST START FILE
-cp ./start_can2rest_template.sh ./start_can2rest.sh
-chmod +x ./start_can2rest.sh
-sed -i -e 's|_CAN2RESTDIR_|'$CAN2RESTDIR'|' ./start_can2rest.sh
